13b9d5b80c9a64636b8dc130348dad1ca6f60f2e
mike <60325499+leguim-repo@users.noreply.github.com>
2020-07-26 18:30:09 +0200

    Codificada la opcion de Ver historico cuenta

--- sgdbBanco/src/main/java/com/miguel/seatcode/bootcamp/sgdb/banco/gui/InterfaceGUI.java
+++ sgdbBanco/src/main/java/com/miguel/seatcode/bootcamp/sgdb/banco/gui/InterfaceGUI.java
@@ -61,7 +61,12 @@ public class InterfaceGUI {
     Historico historico;
     InsertDatabase insertDB = null;
     Scanner reader = null;
-    List<Map<String, Object>> datosConsulta, datosCuenta, datosUsuario, datosHistorico = null;
+    List<Map<String, Object>> datos,datosConsulta, datosCuenta, datosUsuario, datosHistorico = null;
+    String numeroCuenta=null;
+    String cantidadReintegro=null;
+    String cantidadIngreso=null;
+    Integer nuevoBalance=null;
+
         switch (option) {
 
             case 0:
@@ -70,9 +75,9 @@ public class InterfaceGUI {
 
             case 1:
                 // Crear Usuario obtener datos del usuario a traves de la consola
+                // todo se deberia recodifica para usar el EngineSQL
                 usuario = new Usuario();
                 String sqlUsuario=usuario.crearUsuario();
-
 
                 if (sqlUsuario !=null) {
                     //Tengo todos los datos ok los meto en la db
@@ -123,14 +128,23 @@ public class InterfaceGUI {
                 }
 
                 usuario.consultarUsuario(condb);
-
                 break;
 
             case 3:
                 // Reintegro en cuenta
+                // Inicializamos variables
+                /*
+                Este codigo inicialmente deberia ir en el metodo CuentaActions.ingreso
+                pero ahora creo que es mejor que las clases Cuenta, Usuario, Historico fuesen clases sin metodos
+                Las sentencias sql inicialmente las pense generar en esas clases, pero al crear una clase solo para
+                la gestion de sql, lo de generar el sql en las clases no me parece tan interesante
+                de hecho List<Map<String, Object>> deberia ser sustituido por un array de cada clase
+                Aunque de esta forma si que es cierto que se entregan los datos en un formato "universal"
+
+                */
                 reader = new Scanner(System.in);
-                String numeroCuenta=null;
-                String cantidadReintegro=null;
+                numeroCuenta=null;
+                cantidadReintegro=null;
 
                 try {
                     System.out.println("Dame el id de cuenta en el que hacer el reintegro:");
@@ -144,7 +158,7 @@ public class InterfaceGUI {
                         System.out.println("Cantidad a reintegrar:");
                         cantidadReintegro=reader.nextLine();
                         String balance=((HashMap) datosCuenta.toArray()[0]).get("balance").toString();
-                        Integer nuevoBalance= Integer.valueOf(balance) - Integer.valueOf(cantidadReintegro);
+                        nuevoBalance= Integer.valueOf(balance) - Integer.valueOf(cantidadReintegro);
                         // actualizamos los datos de la cuenta
                         misql.updateDatabase("UPDATE cuentas SET balance = "+nuevoBalance.toString()+", fecha = CURRENT_TIMESTAMP WHERE ( id_cuenta  = "+numeroCuenta+")");
                         //creo el registro en el historico
@@ -164,15 +178,63 @@ public class InterfaceGUI {
 
             case 4:
                 // Ingreso en cuenta
-                break;
+                // Inicializamos variables
+                reader = new Scanner(System.in);
+                numeroCuenta=null;
+                cantidadIngreso=null;
 
+                try {
+                    System.out.println("Dame el id de cuenta en el que hacer el ingreo:");
+                    numeroCuenta=reader.nextLine();
+                    datosCuenta=misql.getValuesDatabase("SELECT * from cuentas WHERE( id_cuenta = '"+numeroCuenta+"')");
+                    datosUsuario=misql.getValuesDatabase("SELECT * from usuarios WHERE( id_usuario = '"+((HashMap) datosCuenta.toArray()[0]).get("usuarios_id_usuario")+"')");
+                    imprimirDatos("Datos Cuenta",datosCuenta);
+                    imprimirDatos("Datos Usuario", datosUsuario);
+                    if (bucleSioNo("Esta es la cuenta donde desea hacer el ingreso?")) {
+                        //preguntar cantidad a ingresar
+                        System.out.println("Cantidad a ingresar:");
+                        cantidadIngreso=reader.nextLine();
+                        String balance=((HashMap) datosCuenta.toArray()[0]).get("balance").toString();
+                        nuevoBalance= Integer.valueOf(balance) + Integer.valueOf(cantidadIngreso);
+                        // actualizamos los datos de la cuenta
+                        misql.updateDatabase("UPDATE cuentas SET balance = "+nuevoBalance.toString()+", fecha = CURRENT_TIMESTAMP WHERE ( id_cuenta  = "+numeroCuenta+")");
+                        //creo el registro en el historico
+                        misql.updateDatabase("INSERT INTO historicos ( movimiento,  balance, fecha, cuentas_id_cuenta) VALUES ('Ingreso', "+nuevoBalance.toString()+", CURRENT_TIMESTAMP, "+numeroCuenta+")");
+                    }
+                    else {
+                        System.out.println("Operacion de ingreso cancelada");
+                    }
+                }
+
+                catch (InputMismatchException exception) {
+                    System.out.println("Error al introducir datos de usuario " + exception);
+                    reader.next();
+                }
+
+                break;
             case 5:
-                // Ver Movimientos cuenta
+                // Ver historico cuenta
+                reader = new Scanner(System.in);
+                numeroCuenta=null;
+                datos=null;
+
+                try {
+                    System.out.println("\nDame el ID de la cuenta:");
+                    numeroCuenta=reader.nextLine();
+
+                } catch (InputMismatchException exception) {
+                    System.out.println("Error al introducir el numero de ID de la cuenta " + exception);
+                    reader.next();
+                }
+                5
+                datos = misql.getValuesDatabase("SELECT * FROM historicos WHERE (cuentas_id_cuenta = 1 )");
+                imprimirDatos("Historico de la cuenta",datos);
+
                 break;
 
             case 6:
                 // Ver todas los usuarios tambien le pongo cuentas para probar la funcionalidad del metodo getValuesDatabase
-                List<Map<String, Object>> datos;
+                datos=null;
                 datos = misql.getValuesDatabase("SELECT * FROM usuarios");
                 imprimirDatos("Listado de usuarios",datos);
 
@@ -199,8 +261,8 @@ public class InterfaceGUI {
                 "( 2 ) Consultar Usuario\n"+
                 "( 3 ) Reintegro en cuenta\n"+
                 "( 4 ) Ingreso en cuenta\n"+
-                "( 5 ) Ver Movimientos cuenta\n"+
-                "( 6 ) Ver Todas las cuentas\n"+
+                "( 5 ) Ver historico cuenta\n"+
+                "( 6 ) Ver todos los usuarios y cuentas\n"+
 
                 "\n( 0 ) Salir\n"+
                 "------------------------\n" +
